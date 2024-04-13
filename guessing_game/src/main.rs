fd32449e1d9a32849650b12e2a1c8a1931e12e8d
V. Ganesh <tovganesh@acm.org>
2024-04-13 08:34:53 +0530

    rust autoformat (#3)

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,22 +11,22 @@ fn main() {
         println!("Please input your guess.");
 
         let mut guess = String::new();
-    
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-    
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-    
+
         println!("You guessed: {guess}");
-    
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => { 
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }
--- restapi/src/main.rs
+++ restapi/src/main.rs
@@ -36,4 +36,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         });
     }
 }
-
